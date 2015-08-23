612461bda3692f53315a0ac6dd7746937b208023
Tom Najdek <tom@doppnet.com>
2015-08-23 16:21:22 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ config = {
     'author': 'Tom Najdek',
     'url': 'https://github.com/tnajdek/rafiki',
     'author_email': 'tom@doppnet.com',
-    'version': '0.4.0',
+    'version': '0.4.1',
     'packages': ['rafiki'],
     'install_requires': ['future']
 }
